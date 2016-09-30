351bdd5cfd8fee32174ccf02fa4262cb2c93f0de
comphead <comphead@ukr.net>
2016-09-30 12:21:46 +0300

    assignment1

---
+++ assignment1-starter-code/app.js
@@ -0,0 +1,18 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.output = "";
+  $scope.input = "";
+
+  $scope.calcFoodStatus = function () {
+
+    $scope.output = $scope.input ? ($scope.input.split(',').length <=3 ? "Enjoy!" : "Too much!") : "Please enter data first";
+  };
+}
+
+})();
